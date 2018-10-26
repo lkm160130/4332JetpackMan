816f566d6d41954ed130a915ff63e2f9886f14f3
lkm160130 <38541290+lkm160130@users.noreply.github.com>
2018-10-26 18:47:57 -0500

    Started New Overworld Scene

---
+++ JetpackManProject/Assets/cameraFollowScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowScript : MonoBehaviour {
+
+    Vector3 vctr = new Vector3(0, 0, -10);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = GameObject.Find("Barry").transform.position + vctr;
+	}
+}
