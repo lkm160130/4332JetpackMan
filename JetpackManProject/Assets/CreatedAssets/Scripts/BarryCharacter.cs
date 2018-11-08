f4f15242da38d731b4ab57f2bf1f13d4c89713d1
lkm160130 <38541290+lkm160130@users.noreply.github.com>
2018-11-08 17:44:54 -0600

    blub

--- JetpackManProject/Assets/CreatedAssets/Scripts/BarryCharacter.cs
+++ JetpackManProject/Assets/CreatedAssets/Scripts/BarryCharacter.cs
@@ -57,7 +57,7 @@ namespace UnityStandardAssets._2D
             Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
             for (int i = 0; i < colliders.Length; i++)
             {
-                if (colliders[i].gameObject != gameObject)
+				if (colliders[i].gameObject.tag == "Environment")
                     m_Grounded = true;
             }
             m_Anim.SetBool("Ground", m_Grounded);
